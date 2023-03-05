97adc247e69df8fb82001f0cddde41e4a49a3580
anmolchauhan0610 <anmol.chauhan0610@gmail.com>
2023-03-05 14:27:15 +0530

    title: git command

---
+++ GitPractise.js
@@ -0,0 +1,8 @@
+  
+
+  const handleChange =()=>{
+    var data = document.getElementById("mainheading");
+     document.getElementById("divid").style.background = "green";
+    // console.log("data is ",newDivData);
+    
+  }
